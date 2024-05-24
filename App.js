3771f90517e355347b64c8f8b7cd08dcdfd166ac
Maxime Julliant <maybiz@MacBook-Pro-de-Maxime.local>
2024-05-24 07:30:38 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import Home from './pages/Home/Home'
+import { ImageBackground } from 'react-native'
+import backgroundImage from './assets/background.png'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<ImageBackground source={backgroundImage} className="flex-1 bg-black" imageStyle={{ opacity: 0.75 }}>
+			<SafeAreaProvider>
+				<SafeAreaView className="flex-1">
+					<Home />
+				</SafeAreaView>
+			</SafeAreaProvider>
+		</ImageBackground>
+	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+
+const Home = () => {
+	return (
+		<>
+			<View className="flex-[2]">
+				<Text>Test</Text>
+			</View>
+			<View className="flex-[2]">
+				<Text>Test</Text>
+			</View>
+			<View className="flex-1">
+				<Text>Test</Text>
+			</View>
+		</>
+	)
+}
+export default Home
